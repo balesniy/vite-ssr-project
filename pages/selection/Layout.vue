6e332f5a23e664fc69dac15ea3aaabf2aad4ec04
balesniy <balesniy@yandex.ru>
2023-01-02 14:35:48 +0100

    flex

--- pages/selection/Layout.vue
+++ pages/selection/Layout.vue
@@ -1,17 +1,15 @@
 <template>
-  <div :class="{'block-scroll': isOpen}">
+  <div class="page-wrapper" :class="{'block-scroll': isOpen}">
     <header class="header">
-      <div class="mobile-nav">
-        <a href=""><img class="header-logo" src="./assets/logo-dark.svg" alt=""></a>
-        <div class="buttons">
-          <button class="button chat"><img src="./assets/icon-chat.svg" alt=""></button>
-          <button class="button call"><img src="./assets/icon-phone.svg" alt=""></button>
-          <button :class="{'is-open': isOpen}" class="button burger" @click="isOpen=!isOpen">
-            <span class="burger__line"></span>
-            <span class="burger__line"></span>
-            <span class="burger__line"></span>
-          </button>
-        </div>
+      <a href=""><img class="header-logo" src="./assets/logo-dark.svg" alt=""></a>
+      <div class="buttons">
+        <button class="button chat"><img src="./assets/icon-chat.svg" alt=""></button>
+        <button class="button call"><img src="./assets/icon-phone.svg" alt=""></button>
+        <button :class="{'is-open': isOpen}" class="button burger" @click="isOpen=!isOpen">
+          <span class="burger__line"></span>
+          <span class="burger__line"></span>
+          <span class="burger__line"></span>
+        </button>
       </div>
       <div class="menu-content" :class="{'is-open': isOpen}">
         <ul class="menu-items">
@@ -20,12 +18,24 @@
           <li class="sm-hidden">item 3</li>
           <li>item 4</li>
           <li>item 5</li>
-          <li>item 6</li>
+          <li>
+            <div class="dropdown" :class="{'is-open': isDropdownOpen}">
+              <button class="btn" @click="isDropdownOpen=!isDropdownOpen"><span>€</span> EUR <svg width="12" height="12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.8 7.5 6 3.7 2.2 7.5" stroke="#475467" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg></button>
+              <ul class="dropdown-options">
+                <li><span>$</span> USD</li>
+                <li><span>€</span> EUR</li>
+                <li><span>zł</span> Polish zloty</li>
+                <li><span>₽</span> Rus Ruble</li>
+              </ul>
+            </div>
+          </li>
           <li>item 7</li>
         </ul>
       </div>
     </header>
-    <slot/>
+    <main><slot/></main>
     <footer>page footer</footer>
   </div>
 </template>
@@ -33,10 +43,55 @@
 export default {
   data: () => ({
     isOpen: false,
+    isDropdownOpen: false
   }),
 }
 </script>
 <style scoped lang="scss">
+.dropdown {
+  .btn {
+    display: flex;
+    align-items: center;
+    padding: 5px 8px;
+    gap: 4px;
+    font-size: 14px;
+    line-height: 20px;
+
+    //width: 80px;
+    //height: 30px;
+
+    /* Gray/100 */
+    background: #F2F4F7;
+    border-radius: 8px;
+    border: none;
+    cursor: pointer;
+  }
+  .dropdown-options {
+    margin: 20px 0 0;
+    padding: 0;
+    list-style: none;
+    display: none;
+    & > * {
+      padding: 6px 8px;
+    }
+    @media (min-width: 768px) {
+      position: absolute;
+      background: #FFFFFF;
+      /* Shadows/xl */
+      box-shadow: 0px 5px 24px -4px rgba(16, 24, 40, 0.15), 0px 15px 10px -4px rgba(16, 24, 40, 0.02);
+      border-radius: 8px;
+      padding: 12px;
+    }
+  }
+  &.is-open .dropdown-options{
+    display: flex;
+    gap: 8px;
+    flex-wrap: wrap;
+  }
+}
+.page-wrapper {
+  padding-top: 52px;
+}
 @media (min-width: 768px) {
   .container {
     max-width: 768px;
@@ -59,50 +114,47 @@ export default {
   height: 100vh;
   height: 100dvh;
   overflow-y: hidden;
+  &::before {
+    content: '';
+    position: absolute;
+    background-color: rgba(0,0,0,.2);
+    inset: 0;
+  }
 }
 
 .header {
-  position: relative;
-  box-shadow: 0px 1px 4px rgba(16, 24, 40, 0.08);
-  @media (min-width: 768px) {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-}
-
-.mobile-nav {
+  position: absolute;
+  top: 0;
   display: flex;
+  row-gap: 12px;
+  width: 100%;
+  max-width: 1280px;
+  margin-left: auto;
+  margin-right: auto;
+  flex-wrap: wrap;
   justify-content: space-between;
   align-items: center;
   padding: 16px 12px 12px;
+  background-color: white;
+  box-shadow: 0px 1px 4px rgba(16, 24, 40, 0.08);
 }
 
 .menu-content {
-  position: absolute;
-  left: 0;
-  right: 0;
+  display: none;
   width: 100%;
-  height: calc(100vh - 52px);
-  transform: translateX(-100%);
-  opacity: 0;
-  transition-property: all;
-  transition-duration: 300ms;
-  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-  background-color: rgba(0,0,0,.2);
 
   &.is-open {
-    opacity: 1;
-    transform: translateX(0);
+    display: unset;
   }
 
   @media (min-width: 768px) {
-    position: relative;
-    opacity: 1;
-    transform: translateX(0);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    order: 1;
     background-color: transparent;
-    height: auto;
     width: auto;
+    height: auto;
   }
 }
 
@@ -120,6 +172,7 @@ export default {
     background-color: transparent;
     flex-direction: row;
     align-items: center;
+    padding: 0;
   }
 }
 
@@ -129,7 +182,7 @@ export default {
   justify-content: space-between;
   align-items: center;
   @media screen and (min-width: 768px) {
-    display: none;
+    order: 2;
   }
 }
 
@@ -199,6 +252,9 @@ export default {
         transform: rotate(-45deg);
       }
     }
+  }
+  @media (min-width: 768px) {
+    display: none;
   }
 }
 
